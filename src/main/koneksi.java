bd75a85728c95426cb8f9ab8d375665ae81d069c
ramunir1306 <munirabdul0605@gmail.com>
2025-07-19 03:04:56 +0700

    semi akhir

---
+++ src/main/koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Pongo
+ */
+public class koneksi {
+    private static Connection mysqlconfig;
+    
+    //Method static untuk membuka koneksi ke database MySQL
+    public static Connection konek() {
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/tokoulfa";
+            String user = "root";
+            String pass = "";
+            mysqlconfig=DriverManager.getConnection(url, user, pass);
+        } catch (SQLException sQLException) {
+            System.err.println(sQLException.getMessage());
+        }
+        return mysqlconfig;
+    } 
+}
